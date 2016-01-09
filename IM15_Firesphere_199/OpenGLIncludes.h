ba99f5436f0c3683a6ad9f2d9f2c82495979a334
Roman Luks <rluks@seznam.cz>
2016-01-09 22:02:41 +0100

    comments in glerror

--- IM15_Firesphere_199/OpenGLIncludes.h
+++ IM15_Firesphere_199/OpenGLIncludes.h
@@ -1,4 +1,5 @@
 #pragma once
 
+//some includes for opengl
 #include "..\packages\nupengl.core.0.1.0.1\build\native\include\GL\glew.h" 
 #include "..\packages\nupengl.core.0.1.0.1\build\native\include\GL\freeglut.h" 
